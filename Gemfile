b05aed58d362ac90412d0f464603a5470e094b30
yoitete <>
2025-10-07 22:38:33 +0900

    image_prosessingの追加

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem "rack-cors"
 # AWS SDK for S3
 gem "aws-sdk-s3", require: false
 
+# Image processing for Active Storage
+gem "image_processing", ">= 1.2"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw mswin x64_mingw ], require: "debug/prelude"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,10 @@ Rails.application.configure do
 
   # Require AWS SDK for S3
   require "aws-sdk-s3"
+  
+  # Set default URL options for Active Storage
+  config.action_controller.default_url_options = { host: "api.moko-moko.link", protocol: "https" }
+  Rails.application.routes.default_url_options = { host: "api.moko-moko.link", protocol: "https" }
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
