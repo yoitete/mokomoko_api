7ef69e3393250a18bd66ebae1d89b3692094fa88
yoitete <>
2025-10-07 23:50:45 +0900

    corsエラー解消

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://127.0.0.1:3000", "https://moko-moko.link", "https://api.moko-moko.link", "https://*.dkwmoj6vymm3b.amplifyapp.com"
+    origins "http://localhost:3000", "http://127.0.0.1:3000", "https://moko-moko.link", "https://api.moko-moko.link", /https:\/\/.*\.dkwmoj6vymm3b\.amplifyapp\.com/
 
     resource "*",
       headers: :any,
